e1ed50aaef3b5fcf4a2c2f5b75727d1dff8fdc8b
atlasmoth <ebuka422@gmail.com>
2021-07-02 20:30:11 +0100

    added mongo db

---
+++ pages/account.js
@@ -0,0 +1,26 @@
+import { useUser } from "@auth0/nextjs-auth0";
+import { connectToDatabase } from "./../utils/db";
+import { getSession } from "@auth0/nextjs-auth0";
+
+export default function Account() {
+  const { user, error } = useUser();
+
+  if (error) return <div>{error.message}</div>;
+
+  if (user) {
+    console.log(user);
+    return (
+      <div>
+        Welcome {user.name}! <a href="/api/auth/logout">Logout</a>
+      </div>
+    );
+  }
+  return <h3>Hello kids</h3>;
+}
+
+export async function getServerSideProps(ctx) {
+  console.log(ctx.req);
+  return {
+    props: {},
+  };
+}
