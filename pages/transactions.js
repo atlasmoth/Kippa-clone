17e5282a1b3bd272bd2496a1a5c52d5cbcd4ea4f
atlasmoth <ebuka422@gmail.com>
2021-07-06 19:22:33 +0100

    adding more features

--- pages/transactions.js
+++ pages/transactions.js
@@ -2,20 +2,65 @@ import { useEffect, useState } from "react";
 import axios from "axios";
 import "react-date-range/dist/styles.css";
 import "react-date-range/dist/theme/default.css";
+import { DateRangePicker } from "react-date-range";
+import { addDays } from "date-fns";
+import { connectToDatabase } from "./../utils/db";
+import { getSession } from "@auth0/nextjs-auth0";
 
-export default function Transactions() {
-  const [date, setDate] = useState(new Date().toISOString().split("T")[0]);
+export default function Transaction({ docs }) {
+  const [state, setState] = useState([
+    {
+      startDate: new Date(),
+      endDate: addDays(new Date(), 7),
+      key: "selection",
+    },
+  ]);
   useEffect(() => {
-    console.log(date);
-  }, [date]);
+    const [{ startDate, endDate }] = state;
+    // console.log(new Date(startDate));
+    console.log(new Date(startDate.toDateString()));
+  }, [state]);
   return (
-    <div className="transactions">
-      <div className="container">
-        <div>
-          <label htmlFor="date">Select Range</label>
-          <input type="date" name="" id="" />
-        </div>
-      </div>
-    </div>
+    <DateRangePicker
+      onChange={(item) => setState([item.selection])}
+      showSelectionPreview={true}
+      moveRangeOnFirstSelection={false}
+      months={1}
+      ranges={state}
+      direction="vertical"
+      scroll={{ enabled: true }}
+      maxDate={new Date()}
+      minDate={docs?.date ? new Date(docs.date) : new Date()}
+    />
   );
+}
+
+export async function getServerSideProps(ctx) {
+  const { user } = getSession(ctx.req, ctx.res);
+  const { db } = await connectToDatabase();
+  const [docs] = await db
+    .collection("transactions")
+    .aggregate([
+      {
+        $match: {
+          creator: user.sub,
+        },
+      },
+      {
+        $sort: {
+          date: 1,
+        },
+      },
+      {
+        $limit: 1,
+      },
+    ])
+    .toArray();
+
+  return {
+    props: {
+      user,
+      docs: JSON.parse(JSON.stringify(docs)),
+    },
+  };
 }
