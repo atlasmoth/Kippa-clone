52108de1e0849768d9d182c79c9637e916d5e46e
atlasmoth <ebuka422@gmail.com>
2021-07-09 23:08:16 +0100

    added debt page

--- pages/debt/[...id].js
+++ pages/debt/[...id].js
@@ -1,14 +1,66 @@
+import { ObjectId } from "mongodb";
 import { connectToDatabase } from "./../../utils/db";
 export default function Debt({ data }) {
-  return <div className="debt">{JSON.stringify(data, null, 2)}</div>;
+  const [
+    {
+      clientele: [{ name, phone }],
+      item,
+      amount,
+      date,
+    },
+  ] = data;
+
+  return (
+    <div className="cover">
+      <div className="debt">
+        <h3>
+          {name}'s Balance - <span>&#x20A6;{amount}</span>
+        </h3>
+        <h4>
+          Item - <span>{item}</span>
+        </h4>
+
+        <h4>
+          Due on - <span>{new Date(date).toDateString()}</span>
+        </h4>
+      </div>
+    </div>
+  );
 }
 
 export async function getServerSideProps(ctx) {
   const { params } = ctx;
+
   try {
     const { db } = await connectToDatabase();
 
-    const docs = await db.collection("debt").findOne({ _id: params.id });
+    const docs = await db
+      .collection("debt")
+      .aggregate([
+        {
+          $match: {
+            _id: ObjectId(params.id[0]),
+          },
+        },
+        {
+          $lookup: {
+            from: "customers",
+            let: { customerId: "$customer" },
+            pipeline: [
+              { $addFields: { userId: { $toString: "$_id" } } },
+              {
+                $match: {
+                  $expr: {
+                    $eq: ["$userId", "$$customerId"],
+                  },
+                },
+              },
+            ],
+            as: "clientele",
+          },
+        },
+      ])
+      .toArray();
 
     return {
       props: {
