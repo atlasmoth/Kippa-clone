3f2d7dc0e7ae51c0ee3d596ee3c3e0e653b877c7
atlasmoth <ebuka422@gmail.com>
2021-07-09 20:05:14 +0100

    centralize navbar

--- components/navbar.js
+++ components/navbar.js
@@ -5,7 +5,7 @@ import { makeStyles } from "@material-ui/core/styles";
 import AppBar from "@material-ui/core/AppBar";
 import Toolbar from "@material-ui/core/Toolbar";
 import Typography from "@material-ui/core/Typography";
-import Button from "@material-ui/core/Button";
+import Box from "@material-ui/core/Box";
 import LibraryBooksIcon from "@material-ui/icons/LibraryBooks";
 import PeopleIcon from "@material-ui/icons/People";
 import HistoryIcon from "@material-ui/icons/History";
@@ -29,36 +29,38 @@ export default function Navbar() {
     <div className={classes.root}>
       <AppBar position="static">
         <Toolbar>
-          <Typography variant="h6" className={classes.title}>
-            <Link href="/account">
-              <a style={{ color: "#fff", textDecoration: "none" }}>
-                <LibraryBooksIcon />
-              </a>
-            </Link>
-          </Typography>
-          <Typography variant="h6" className={classes.title}>
-            <Link href="/customers">
-              <a style={{ color: "#fff", textDecoration: "none" }}>
-                <PeopleIcon />
-              </a>
-            </Link>
-          </Typography>
+          <Box display="flex" justifyContent="space-around" alignItems="center">
+            <Typography variant="h6" className={classes.title}>
+              <Link href="/account">
+                <a style={{ color: "#fff", textDecoration: "none" }}>
+                  <LibraryBooksIcon />
+                </a>
+              </Link>
+            </Typography>
+            <Typography variant="h6" className={classes.title}>
+              <Link href="/customers">
+                <a style={{ color: "#fff", textDecoration: "none" }}>
+                  <PeopleIcon />
+                </a>
+              </Link>
+            </Typography>
 
-          <Typography variant="h6" className={classes.title}>
-            <Link href="/transactions">
-              <a style={{ color: "#fff", textDecoration: "none" }}>
-                <HistoryIcon />
+            <Typography variant="h6" className={classes.title}>
+              <Link href="/transactions">
+                <a style={{ color: "#fff", textDecoration: "none" }}>
+                  <HistoryIcon />
+                </a>
+              </Link>
+            </Typography>
+            <Typography variant="h6" className={classes.title}>
+              <a
+                style={{ color: "#fff", textDecoration: "none" }}
+                href="/api/auth/logout"
+              >
+                Logout
               </a>
-            </Link>
-          </Typography>
-          <Typography variant="h6" className={classes.title}>
-            <a
-              style={{ color: "#fff", textDecoration: "none" }}
-              href="/api/auth/logout"
-            >
-              Logout
-            </a>
-          </Typography>
+            </Typography>
+          </Box>
         </Toolbar>
       </AppBar>
     </div>
